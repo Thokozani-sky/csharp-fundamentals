b414c963beebb0f4f0ad910aa694ffef416426fa
Thokozani-sky <issacthokozani0@gmail.com>
2025-12-04 06:28:06 +0200

    Add SumTwoNumbers class to calculate sum

---
+++ 1_sum.cs
@@ -0,0 +1,12 @@
+using System;
+
+class SumTwoNumbers 
+{
+    static void Main(string[] args) 
+    {
+      int x = 5;
+      int y = 6;
+      int sum = x + y;
+      Console.WriteLine(sum);
+    }
+}
