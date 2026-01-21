2797299504170a807c70a3cc3c39f36ea91bd55a
Thokozani-sky <issacthokozani0@gmail.com>
2026-01-21 16:34:34 +0200

    Delete 7_palindrome.cs

--- 7_palindrome.cs
+++
@@ -1,9 +0,0 @@
-using System;
-class Program {
-    static void Main() {
-        string s = Console.ReadLine().Replace(" ", "").ToLower();
-        string r = new string(s.ToCharArray().Reverse().ToArray());
-        if (s == r) Console.WriteLine("Palindrome");
-        else Console.WriteLine("Not a palindrome");
-    }
-}
