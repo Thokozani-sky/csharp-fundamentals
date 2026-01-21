575706f50aead1b09cba5a694425cec822dcc383
Thokozani-sky <issacthokozani0@gmail.com>
2026-01-21 16:32:25 +0200

    Delete 2_avg.cs

--- 2_avg.cs
+++
@@ -1,9 +0,0 @@
-using System;
-class Program {
-    static void Main() {
-        double x = double.Parse(Console.ReadLine());
-        double y = double.Parse(Console.ReadLine());
-        double z = double.Parse(Console.ReadLine());
-        Console.WriteLine("Average: " + ((x + y + z) / 3).ToString("F2"));
-    }
-}
