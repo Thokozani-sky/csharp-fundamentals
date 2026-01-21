30c8f4a1a1d89658700d0e280b3e6870210715ba
Thokozani-sky <issacthokozani0@gmail.com>
2026-01-21 16:30:25 +0200

    Delete 16_words.cs

--- 16_words.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-class Program {
-    static void Main() {
-        string text = Console.ReadLine();
-        string[] words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-        Console.WriteLine(words.Length);
-    }
-}
