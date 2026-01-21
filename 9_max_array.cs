8bf20079f825366d0c14c6254f630d8d0ef38a53
Thokozani-sky <issacthokozani0@gmail.com>
2026-01-21 16:35:53 +0200

    Delete 9_max_array.cs

--- 9_max_array.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using System.Linq;
-class Program {
-    static void Main() {
-        int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-        Console.WriteLine("Max: " + nums.Max());
-    }
-}
