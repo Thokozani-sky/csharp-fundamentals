4348c4d903c57103c96d934ff6f4f0b0b60b2b21
Thokozani-sky <issacthokozani0@gmail.com>
2026-01-21 16:33:13 +0200

    Delete 4_factorial.cs

--- 4_factorial.cs
+++
@@ -1,9 +0,0 @@
-using System;
-class Program {
-    static void Main() {
-        int n = int.Parse(Console.ReadLine());
-        int f = 1;
-        for (int i = 1; i <= n; i++) f *= i;
-        Console.WriteLine(n + "! = " + f);
-    }
-}
